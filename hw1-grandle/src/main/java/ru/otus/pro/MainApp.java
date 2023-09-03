402444aac33aeb6725c4ecd2b50d4f1fccd1e91a
AlexonJ <mfa#8sQY>
2023-09-03 21:15:28 +0400

    hw1-grandle initial commit

---
+++ hw1-grandle/src/main/java/ru/otus/pro/MainApp.java
@@ -0,0 +1,9 @@
+package ru.otus.pro;
+
+import java.util.Arrays;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println(String.format("Hello! I'm running with args: %s", Arrays.toString(args)));
+    }
+}
