cc62f7deb9ee797d56335863a5ef504e1fe77085
AlexonJ <FMailP@yandex.ru>
2023-11-07 15:50:06 +0400

    Homework 11 - JPQL. Applying template without any changes

---
+++ hw11-jpql/src/main/java/ru/otus/javapro/homeworks/hw11jpql/core/sessionmanager/DataBaseOperationException.java
@@ -0,0 +1,7 @@
+package ru.otus.javapro.homeworks.hw11jpql.core.sessionmanager;
+
+public class DataBaseOperationException extends RuntimeException {
+    public DataBaseOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
