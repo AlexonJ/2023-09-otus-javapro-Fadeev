402f02b17fc4780ca46cfce8dbc8780e1a32842f
AlexonJ <FMailP@yandex.ru>
2023-11-07 16:24:40 +0400

    Homework 11 - hibernate. Preparing template

--- hw11-jpql/src/main/java/ru/otus/javapro/homeworks/hw11jpql/core/sessionmanager/DataBaseOperationException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.javapro.homeworks.hw11jpql.core.sessionmanager;
-
-public class DataBaseOperationException extends RuntimeException {
-    public DataBaseOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
