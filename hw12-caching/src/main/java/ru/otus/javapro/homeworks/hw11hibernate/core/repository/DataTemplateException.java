7f0c0024186fbba455f2269a5802d5136fc1cbd7
AlexonJ <FMailP@yandex.ru>
2023-11-18 18:24:21 +0400

    Homework 12 - caching. Applying template from the last homework (homework 11 - Hibernate)

---
+++ hw12-caching/src/main/java/ru/otus/javapro/homeworks/hw11hibernate/core/repository/DataTemplateException.java
@@ -0,0 +1,7 @@
+package ru.otus.javapro.homeworks.hw11hibernate.core.repository;
+
+public class DataTemplateException extends RuntimeException {
+    public DataTemplateException(Exception ex) {
+        super(ex);
+    }
+}
